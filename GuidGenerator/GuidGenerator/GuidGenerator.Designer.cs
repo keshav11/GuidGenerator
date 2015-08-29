99f5944e34d26056bbbd760a0e9607cf43ad7c23
Keshav Gupta <keshav1032@gmail.com>
2015-08-29 11:54:31 +0530

    Added winform app

---
+++ GuidGenerator/GuidGenerator/GuidGenerator.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace GuidGenerator
+{
+    partial class GuidGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGuidGenerator = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonGuidGenerator
+            // 
+            this.buttonGuidGenerator.Location = new System.Drawing.Point(12, 163);
+            this.buttonGuidGenerator.Name = "buttonGuidGenerator";
+            this.buttonGuidGenerator.Size = new System.Drawing.Size(127, 46);
+            this.buttonGuidGenerator.TabIndex = 1;
+            this.buttonGuidGenerator.Text = "Generate Guid";
+            this.buttonGuidGenerator.UseVisualStyleBackColor = true;
+            this.buttonGuidGenerator.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(342, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 46);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Copy Guid";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // GuidGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 303);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonGuidGenerator);
+            this.Name = "GuidGenerator";
+            this.Text = "Guid Generator";
+            this.Load += new System.EventHandler(this.GuidGenerator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonGuidGenerator;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
